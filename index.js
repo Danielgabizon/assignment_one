eeab00785a6d8f444b08c3ff768f2e1f64cca85b
Daniel <daniel.gabizon1105@gmail.com>
2024-11-17 17:49:57 +0200

    change

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const dotevn = require("dotenv").config();
 const port = process.env.PORT;
-
+const mongoose = require("mongose");
+mongoose.connect(process.env.DB_CONNECT, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function () {
+  console.log("Connected dto database");
+});
 const posts_routes = require("./routes/posts_routes");
 app.use("/posts", posts_routes);
 
