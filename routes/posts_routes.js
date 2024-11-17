17895b1fefab1935a49ae59551adc8ab75147acb
Daniel <daniel.gabizon1105@gmail.com>
2024-11-17 17:37:43 +0200

    change

--- routes/posts_routes.js
+++ routes/posts_routes.js
@@ -1,8 +1,11 @@
+// importing Express and creating a Router instance:
 const express = require("express");
 const router = express.Router();
+// importing the controller functions that will handle the logic for each route.
 const postController = require("../controllers/posts_controller");
+// setting Up the routes:
 router.get("/", postController.getAllPosts);
 router.post("/", postController.createPost);
 router.delete("/", postController.deletePost);
-
+//exporting the router so that it can be imported and used in other parts of your application. F
 module.exports = router;
