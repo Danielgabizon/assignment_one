5c13def9a971e1ee96d2951021065d2c6fe685a7
Daniel <daniel.gabizon1105@gmail.com>
2024-11-23 21:16:07 +0200

    posts changes

--- routes/posts_routes.js
+++ routes/posts_routes.js
@@ -4,11 +4,10 @@ const router = express.Router();
 // importing the controller functions that will handle the logic for each route.
 const postController = require("../controllers/posts_controller");
 // setting Up the routes:
+router.post("/", postController.addNewPost);
 router.get("/", postController.getAllPosts);
 router.get("/:id", postController.getPostById);
+router.put("/:id", postController.updatePost);
 
-router.post("/", postController.createPost);
-
-router.delete("/:id", postController.deletePost);
 //exporting the router so that it can be imported and used in other parts of application. F
 module.exports = router;
